af0b74cdab1eb2d4b28e758e3678b617b7fa6d31
rajkumardongre <rajkumardongre17@gmail.com>
2022-07-23 14:12:47 +0530

    user model Added

--- user/models/User.js
+++ user/models/User.js
@@ -14,7 +14,161 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: [true, 'Please enter a password'],
     minlength: [6, 'Minimum password length is 6 characters'],
-  }
+  },
+
+  // ============== Personal Information =============================
+
+  //==================== Personal Details =============================
+
+  firstName: {
+    type: String,
+    minlength: [3, 'First Name is required'],
+  },
+  lastName: { type: String, minlength: [3, 'Last Name is required'] },
+  aadharNumber: {
+    type: String
+  },
+  mobileNumber: { type: String },
+  dob: { type: Date },
+  gender: { type: String },
+  fatherName: { type: String },
+  motherName: { type: String },
+  address: { type: String },
+  state: { type: String },
+  district: { type: String },
+  taluka: { type: String },
+  pincode: { type: String },
+  maritalStatus: { type: String },
+
+  //============== Religion and Caste detail ==================
+
+  religion: { type: String },
+  casteCategory: { type: String },
+  caste: { type: String },
+  haveCasteCertificate: { type: Boolean },
+  casteCertificateNumber: { type: String },
+  casteIssuingDistrict: { type: String },
+  casteApplicantName: { type: String },
+  casteIssuingAuthority: { type: String },
+  casteIssuingDate: { type: String },
+  casteCretificate: { type: String },
+  
+  // ============== Income Details =============================
+
+  familyAnnualIncome: { type: Number },
+  haveIncomeCertificate: { type: Boolean },
+  incomeCertificateNumber: { type: Number },
+  incomeIssuingAuthority: { type: String },
+  incomeIssuingDate: { type: Date },
+  incomeCertificate: { type: String },
+  
+  // ============== Domicile Details =============================
+
+  domicileState: { type: String },
+  haveDomicileCertificate: { type: Boolean },
+  domicileCertificateNumber: { type: Number },
+  domicileRelationType: { type: String },
+  domicileIssuingAuthority: { type: String },
+  domicileIssuingDate: { type: Date },
+  domicileCertificate: { type: String },
+  
+  // ============== Personal Eligibility Details =============================
+
+  isSalaried: { type: Boolean },
+  haveDisability: { type: Boolean },
+  disabilityDesc: { type: String },
+  disabilityCertificate: { type: String },
+
+  // ============== Aadhaar Bank Details =============================
+
+  isAadharLinkedToBank: { type: Boolean },
+  
+  // ============== Bank Details =============================
+
+  bankAccountNumber: { type: String },
+  bankIFSCode: { type: String },
+  bankBranch: { type: String },
+  
+  // ============== Other Information =============================
+
+  // ============== Parent's/Guardian's Details =============================
+
+  isFatherAlive: { type: Boolean },
+  fatherOccupation: { type: String },
+  isFatherSalaried: { type: Boolean },
+  
+  isMotherAlive: { type: Boolean },
+  motherOccupation: { type: String },
+  isMotherSalaried: { type: Boolean },
+
+  // ============== Current Course Information =============================
+  currentCourse: [
+    {
+      ccAddmissionYear: { type: Number },
+      ccInstituteState: { type: String },
+      ccInstituteDistrict: { type: String },
+      ccInstituteTaluka: { type: String },
+      ccQualificationLevel: { type: String },
+      ccStream: { type: String },
+      ccCollegeName: { type: String }, 
+      ccCourseName: { type: String },
+      ccAddmissionType: { type: String },
+      ccCETpercentage: { type: mongoose.Types.Decimal128 },
+      ccCETadmitCardNumber: { type: String },
+      ccAdmitCard: { type: String },
+      ccCAPIDcertificate: { type: String },
+      ccYearOfStudy: { type: Number },
+      ccCompletedOrContinue: { type: String },
+      ccIsProfessional: { type: String },
+      ccIsAddmissionThroughOpenOrReserved: { type: String },
+      ccGapYears: { type: Number },
+      ccMode : { type: String },
+    }
+  ],
+
+  // ============== Past Qualifications Information =============================
+
+  pastQualifications: [
+    {
+      pqLevel: { type: String },
+      pqStream: { type: String },
+      pqCompleted: { type: String },
+      pqInstituteState: { type: String },
+      pqInstituteDistrict: { type: String },
+      pqInstituteTaluka: { type: String },
+      pqCollegeName: { type: String },
+      pqCourseName: { type: String },
+      pqBoardUniversityName: { type: String }, 
+      pqMode: { type: String },
+      pqAddmissionYear: { type: Number },
+      pqPassingYear: { type: Number },
+      pqResult: { type: String },
+      pqPercentage: { type: mongoose.Types.Decimal128 },
+      pqAttempts: { type: Number },
+      pqMarksheet: { type: String },  
+      pqHaveGAP: { type: Boolean },
+      pqGAPyears: { type: Number },
+    }
+  ],
+
+  // ============== Hostel Information =============================
+
+  hiBeneficiaryCategory: { type: String },
+  hostelDetails: [
+    {
+      hostelName: { type: String }, 
+      hostelAddress: { type: String },
+      hostelState: { type: String },
+      hostelDistrict: { type: String },
+      hostelTaluka: { type: String },
+      hostelVillage: { type: String },
+      hostelPinCode: { type: String },
+      hostelType: { type: String },
+      hostelAdmissionDate: { type: Date },
+      hostelIsMessAvailable: { type: Boolean },
+      hostelAddmissionLetter: { type: String },
+    }
+  ],
 });
 
 
